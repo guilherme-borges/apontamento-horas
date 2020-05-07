87738bedccfd6c25192a1715a7682782f381df65
Guilherme Borges <guilherme.borges.dev@gmail.com>
2020-05-07 08:52:48 -0300

    Inicio do backend

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(3333, () => {
+    console.log('Servidor rodando na porta 3333')
+})
